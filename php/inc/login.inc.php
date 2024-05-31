90d1abd3b3de000fc244f1a569814b44707905f2
Alumgs <alumgs@majadamarcial.es>
2024-05-31 22:05:16 +0100

    login

--- php/inc/login.inc.php
+++ php/inc/login.inc.php
@@ -1,8 +1,14 @@
 <?php
 
+session_start();
+
 $mysqli = new mysqli("localhost:3307", "root", "", "albergue");
 if (!$mysqli) {
     die("No se pudo conectar a la BBDD: " . mysqli_connect_error());
 }
 
-echo "hola";
+if (isset($_POST['usua'])
+
+$query = 
+
+$_SESSION[''];
