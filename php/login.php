b5db1ccc61edacaa3f98539da9095637a2ed73a5
gastbr <71907959+gastbr@users.noreply.github.com>
2024-06-05 06:11:33 +0100

    Pendiente EDITAR anfitrion y mascota  en inicio_admin
    inicio admin con consulta, edicion y borrado / generar PDFs / filtros en listado público / login de anfitriones / voluntarios y donaciones / info_mascota: añadir mapa  y calendario de visitas / añadir formulario para adoptar a un perri / infinite scroll en inicio visitantes

--- php/login.php
+++ php/login.php
@@ -13,6 +13,12 @@ if (isset($_SESSION['user'])) {
 }
 ?>
 
+<script>
+    // Activar tooltips
+    const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]');
+    const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl));
+</script>
+
 <main class="d-flex justify-content-center">
     <a class="volver d-inline mt-3" href="./index.php">&lt; Volver</a>
 
@@ -22,11 +28,11 @@ if (isset($_SESSION['user'])) {
             <h2 class="d-block">Login de usuarios</h2>
             <form action="./inc/login.inc.php?session=login" method="post">
                 <div class="user my-3 text-left">
-                    <label for="user">Usuario:</label>
+                    <label for="user"> <a href="#" data-bs-toggle="tooltip" data-bs-title="Default tooltip" data-bs-placement="top">Usuario:</a></label>
                     <input class="w-100" type="text" name="user" required>
                 </div>
                 <div class="pass my-3">
-                    <label for="pass">Contraseña:</label>
+                    <label for="pass">Contraseña: (1234)</label>
                     <input class="w-100" type="password" name="pass" required>
                 </div>
                 <button class="btn btn-success btn-sm w-100" type="submit">Acceder</button>
@@ -37,13 +43,15 @@ if (isset($_SESSION['user'])) {
     </section>
 </main>
 
-<?php
-if (isset($_GET['login']) && $_GET['login'] == 'fail') {
-    ?>
-    <script>
-        document.querySelector(".alert").classList.replace("d-none", "d-block");
-    </script>
+<script>
     <?php
-}
+    // Visualiza la alerta de login fail.
+    if (isset($_GET['login']) && $_GET['login'] == 'fail') {
+    ?>
+        document.querySelector(".alert").classList.replace("d-none", "d-block");
+    <?php
+    } ?>
+</script>
+<?php
 FooterHTML();
 ?>
